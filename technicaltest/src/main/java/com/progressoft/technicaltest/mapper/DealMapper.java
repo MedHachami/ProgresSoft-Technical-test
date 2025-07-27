16c2795ca9ef31df18a990508f850e2361fa2f15
MedHachami <moha.hach.9@gmail.com>
2025-07-27 21:33:36 +0100

    fix(mapper) explicit mapping

--- technicaltest/src/main/java/com/progressoft/technicaltest/mapper/DealMapper.java
+++ technicaltest/src/main/java/com/progressoft/technicaltest/mapper/DealMapper.java
@@ -4,10 +4,20 @@ import com.progressoft.technicaltest.dto.DealRequestDto;
 import com.progressoft.technicaltest.dto.DealResponseDto;
 import com.progressoft.technicaltest.entity.Deal;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface DealMapper {
+    @Mapping(target = "fromCurrency", source = "fromCurrency")
+    @Mapping(target = "toCurrency", source = "toCurrency")
+    @Mapping(target = "timestamp", source = "timestamp")
+    @Mapping(target = "amount", source = "amount")
     Deal toEntity(DealRequestDto dto);
 
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "fromCurrency", source = "fromCurrency")
+    @Mapping(target = "toCurrency", source = "toCurrency")
+    @Mapping(target = "timestamp", source = "timestamp")
+    @Mapping(target = "amount", source = "amount")
     DealResponseDto toResponseEntity(Deal deal);
 }
